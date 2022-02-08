7c55987238bf436ee44701d6db6bb9969eebb231
tharuka.ruwan <tharukaruwan2@gmail.com>
2022-02-08 23:52:02 +0530

    models registerd in admin panel

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -1,24 +1,5 @@
 from firebase_admin import auth
 
-# class TockenDecode:
-#     def __init__(self,get_response):
-#         self.get_response
-
-#     def __call__(self,request):
-#         try:
-#             auth_header=request.META.get('HTTP_AUTHORIZATION')
-#             tocken=auth_header.replace('Bearer ','')
-#             decoded_tocken=auth.verify_id_token(tocken)
-#             userid=decoded_tocken['user_id']
-#         except:
-#             response=self.get_response(request)
-#         response=self.get_response(request)
-#         return response
-    
-#     def inject_tocken_data(self,_,response):
-#         response.context_data["tocken_decode"]=self.tocken_decode
-#         return response
-
 def tocken_decode(get_response):
     # One-time configuration and initialization.
 
